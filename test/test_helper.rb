0ef6e53e9443cae5901f178c3388a23520a9c1a7
rbf <rob@sixsq.com>
2015-08-08 18:50:36 +0200

    new: setup CodeClimate code coverage reporting with TravisCI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
+# Suggested by https://travis-ci.org/basimilch/basimilch-app
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
