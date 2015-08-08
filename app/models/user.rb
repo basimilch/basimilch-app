96fb8160c1e1ae308d25804ced6ec2a098c9ac04
rbf <rob@sixsq.com>
2015-08-08 11:35:39 +0200

    new: test phone numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
 
   # Inspired from
   #   https://www.railstutorial.org/book/_single-page#sec-format_validation
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :email,       presence: true,
                           length: { maximum: 255 },
                           format: { with: VALID_EMAIL_REGEX },
@@ -18,6 +20,48 @@ class User < ActiveRecord::Base
   validates :last_name,   presence: true,
                           length: { maximum: 40 }
 
+  # Source:
+  #  http://guides.rubyonrails.org/active_record_validations.html#validates-each
+  validates_each :tel_mobile, :tel_home, :tel_office do |record, attr, value|
+    unless value.blank?
+      # Source: https://github.com/joost/phony_rails
+      normalized_value = value.phony_normalized(default_country_code: 'CH')
+      if value =~ /[[:alpha:]]/ or
+         normalized_value.blank? or
+         !Phony.plausible?(normalized_value)
+        record.errors.add(attr, :improbable_phone)
+      end
+    end
+  end
+  before_save :normalize_tels
+
   after_initialize { self.country = I18n.t :switzerland }
+
   has_secure_password
+
+  def formatted_tel(tel_type)
+    if valid?
+      raw_tel = send "tel_#{tel_type}" || ""
+      # Source:
+      #   https://github.com/floere/phony/blob/master/qed/format.md#options
+      raw_tel.phony_formatted(:normalize => 'CH',
+                              :format => :international, # => "+41 76 111 11 11"
+                              # :format => :national,    # => "076 111 11 11"
+                              :spaces => ' ')
+    end
+  end
+
+  private
+
+    def normalize_tels
+      self.tel_mobile = normalized_tel tel_mobile
+      self.tel_home   = normalized_tel tel_home
+      self.tel_office = normalized_tel tel_office
+    end
+
+    def normalized_tel(tel)
+      unless tel.blank?
+        tel.phony_normalized(default_country_code: 'CH')
+      end
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -82,7 +82,6 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "user@example.com", @user.reload.email
   end
 
-
   test "password and confirmation should be present" do
     @user.password = @user.password_confirmation = " " * 10
     assert_not @user.valid?
@@ -97,12 +96,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-
   test "admin should be false by default" do
     assert_equal false, @user.admin
     assert_equal false, @user.admin?
   end
-
 
   test "first name should be present" do
     @user.first_name = "     "
@@ -114,7 +111,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-
   test "last name should be present" do
     @user.last_name = "     "
     assert_not @user.valid?
@@ -125,10 +121,87 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-
   test "country should be set by default" do
     assert_equal "Schweiz", @user.country
   end
 
+  test "valid phone numbers should be stored normalized" do
+    valid_tels = {"+41761111111"       => ["+41761111111", "+41 76 111 11 11"],
+                  "+41 76 111 11 11"   => ["+41761111111", "+41 76 111 11 11"],
+                  "+41 (0) 76 1111111" => ["+41761111111", "+41 76 111 11 11"],
+                  "0761111111"         => ["+41761111111", "+41 76 111 11 11"],
+                  "076.111.11.11"      => ["+41761111111", "+41 76 111 11 11"],
+                  "076-111-11-11"      => ["+41761111111", "+41 76 111 11 11"],
+                  "+1-202-555-0110"    => ["+12025550110", "+1 (202) 555-0110"],
+                  "0012025550110"      => ["+12025550110", "+1 (202) 555-0110"],
+                  "+33611111111"       => ["+33611111111", "+33 6 11 11 11 11"],
+                  "+33 6 11 11 11 11"  => ["+33611111111", "+33 6 11 11 11 11"],
+                  "0033611111111"      => ["+33611111111", "+33 6 11 11 11 11"],
+                  "0033 6 11 11 11 11" => ["+33611111111", "+33 6 11 11 11 11"]}
+    valid_tels.each do |raw_tel, parsed_tels|
+      normalized_tel  = parsed_tels.first
+      formatted_tel   = parsed_tels.second
+      # Validate all three phone nombers
+      @user.tel_mobile = raw_tel
+      @user.tel_home   = raw_tel
+      @user.tel_office = raw_tel
+      assert @user.valid?, "#{raw_tel.inspect} should be valid: " +
+                           "#{@user.errors.full_messages_for(:tel_mobile)}"
+      # Before saving the user to the DB, the tel is still the raw string
+      assert_equal raw_tel, @user.tel_mobile
+      assert @user.save
+      # After saving the user to the DB, the tel should be normalized
+      @user.reload
+      assert_equal normalized_tel,  @user.tel_mobile
+      # The tel can be retrieved as a formatted string for display
+      assert_equal formatted_tel,   @user.formatted_tel(:mobile)
+    end
+  end
 
+  test "the three phone numbers should be independent" do
+    valid_tels = {mobile: ["076 123 45 67",         "+41 76 123 45 67"],
+                  home:   ["0440101122",            "+41 44 010 11 22"],
+                  office: ["+41 (0) 44 121 12 12",  "+41 44 121 12 12"]}
+    valid_tels.each do |tel_type, tels|
+      @user.send "tel_#{tel_type}=", tels.first
+    end
+    assert @user.save
+    @user.reload
+    valid_tels.each do |tel_type, tels|
+      assert_equal @user.formatted_tel(tel_type), tels.second
+    end
+  end
+
+  test "phone validation should reject invalid phone numbers" do
+    invalid_tels = [
+                    "a",
+                    "user@example.com",
+                    "+41a (0) 76 1111111",  # contains letters
+                    "+41à (0) 76 1111111",  # contains letters
+                    "+41ü (0) 76 1111111",  # contains letters
+                    "+417611111111",        # too short
+                    "+41761111111111",      # too long
+                    "+41 76 11 11 11",      # too short
+                    "+41 76 1111 11 11 11", # too long
+                    "07611111111",          # too short
+                    "0761111111111",        # too long
+                    "076.111.11.11.1",      # too short
+                    "076.111.11.11.111",    # too long
+                    "076-111-11-11-1",      # too short
+                    "076-111-11-11-111",    # too long
+                    "+3361111111",          # too short
+                    "+336111111111",        # too long
+                    "+33 6 11 11 11 1",     # too short
+                    "+33 6 11 11 11 111",   # too long
+                    "+3361111111",          # too short
+                    "+336111111111",        # too long
+                   ]
+    invalid_tels.each do |invalid_tel|
+      @user.tel_mobile = invalid_tel
+      assert_not @user.valid?, "#{invalid_tel.inspect} should be invalid: " +
+                           "#{@user.tel_mobile.inspect} => " +
+                           "#{@user.errors.full_messages_for(:tel_mobile)}"
+      assert_equal nil, @user.formatted_tel(:mobile)
+    end
+  end
 end
