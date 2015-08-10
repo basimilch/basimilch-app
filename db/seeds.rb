1820df973e9d4e88a15fc4472dbc7e37ba2ff4ec
rbf <rob@sixsq.com>
2015-08-10 18:54:07 +0200

    new: make seed.rb file to populate dev DB with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Inspired from: https://www.railstutorial.org/book/_single-page#code-db_seed
+# Apply with:
+#   bundle exec rake db:migrate:reset
+#   bundle exec rake db:seed
+
+puts
+puts "******** Seeding Data Start ************"
+puts
+
+User.create!(first_name:      "User",
+             last_name:       "Example",
+             admin:           true,
+             email:           "user@example.org",
+             postal_address:  "Some Street 42",
+             postal_code:     "8953",
+             city:            "Dietikon",
+             # country:         "", # Use default
+             tel_mobile:      "012 345 67 89",
+             tel_home:        "098 765 43 21",
+             tel_office:      "+41 (0) 11 222 33 44",
+             # status:          "", # Use default
+             notes:           "The first user.") unless User.first
+
+def maybe(str)
+  # Source: http://stackoverflow.com/a/8012789
+  [true, false].sample ? str : ""
+end
+
+
+# Using Faker to generate fake data. :)
+# Source: https://github.com/stympy/faker
+
+separators  = [".", "-", "_", ""]
+locales     = ['de-CH', 'fr-CH', 'it-CH']
+
+100.times do |n|
+  Faker::Config.locale = locales.sample
+
+  first_name  = Faker::Name.first_name
+  last_name   = Faker::Name.last_name
+  username    = Faker::Internet.user_name("#{first_name} #{last_name}",
+                                          separators) +
+                maybe(separators.sample + Faker::Number.number(2).to_s)
+  email       = Faker::Internet.email(username)
+
+  user = User.create!(first_name:     first_name,
+                      last_name:      last_name,
+                      email:          email,
+                      postal_address: maybe(Faker::Address.street_address),
+                      postal_code:    Faker::Number.number(4).to_s,
+                      city:           Faker::Address.city,
+                      # country:        "", # Use default
+                      tel_mobile:     maybe("0" + Faker::Number.number(9).to_s),
+                      tel_home:       maybe("0" + Faker::Number.number(9).to_s),
+                      tel_office:     maybe("0" + Faker::Number.number(9).to_s),
+                      # status:         "", # Use default
+                      notes:          maybe(Faker::Lorem.sentence))
+  puts user.inspect
+end
+
+puts
+puts "DB has now #{User.count} users."
+puts
+puts "******** Seeding Data End ************"
+puts
