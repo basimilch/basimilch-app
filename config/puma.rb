1ad16adc5893f018cb97ee4003812934cf760b79
rbf <rob@sixsq.com>
2015-08-14 09:07:48 +0200

    new: enable usage of Puma web server in production

---
+++ config/puma.rb
@@ -0,0 +1,26 @@
+# This config file gets loaded when starting Puma as the web server in Heroku as
+# configured in the 'Procfile' at the app's root folder.
+# Sources:
+#   https://www.railstutorial.org/book/_single-page#sec-production_webserver
+#   https://devcenter.heroku.com/articles/ruby-default-web-server
+#   https://devcenter.heroku.com/articles/
+#                   deploying-rails-applications-with-the-puma-web-server#config
+#   https://devcenter.heroku.com/articles/
+#                 deploying-rails-applications-with-the-puma-web-server#procfile
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
