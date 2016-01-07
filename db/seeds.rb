9ef96bb48315056b70f4fb361b44b21ad83db1f0
rbf <rbf@users.noreply.github.com>
2016-01-07 18:56:56 +0100

    fix: skip model validations during db seeding
    Because the generated addresses are randon and not real, and now we have
    validation on the addresses. Therefore the seeding was failing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,25 @@ puts
 puts "******** Seeding Data Start ************"
 puts
 
-User.create!(first_name:      "User",
-             last_name:       "Example",
-             admin:           true,
-             email:           "user@example.org",
-             postal_address:  "Some Street 42",
-             postal_code:     "8953",
-             city:            "Dietikon",
-             # country:         "", # Use default
-             tel_mobile:      "012 345 67 89",
-             tel_home:        "098 765 43 21",
-             tel_office:      "+41 (0) 11 222 33 44",
-             # status:          "", # Use default
-             notes:           "The first user.",
-             activated: true,
-             activated_at: Time.current) unless User.first
+unless User.first
+  first_user = User.new(first_name:      "User",
+                        last_name:       "Example",
+                        admin:           true,
+                        email:           "user@example.org",
+                        postal_address:  "Some Street 42",
+                        postal_code:     "8953",
+                        city:            "Dietikon",
+                        # country:         "", # Use default
+                        tel_mobile:      "012 345 67 89",
+                        tel_home:        "098 765 43 21",
+                        tel_office:      "+41 (0) 11 222 33 44",
+                        # status:          "", # Use default
+                        notes:           "The first user.",
+                        activated: true,
+                        activated_at: Time.current)
+  first_user.save(validate: false)
+  puts first_user.inspect
+end
 
 def maybe(str)
   # Source: http://stackoverflow.com/a/8012789
@@ -45,25 +49,29 @@ locales     = ['de-CH', 'fr-CH', 'it-CH']
                 maybe(separators.sample + Faker::Number.number(2).to_s)
   email       = Faker::Internet.email(username)
 
-  user = User.create!(first_name:     first_name,
-                      last_name:      last_name,
-                      email:          email,
-                      postal_address: Faker::Address.street_address,
-                      postal_code:    Faker::Number.number(4).to_s,
-                      city:           Faker::Address.city,
-                      # country:        "", # Use default
-                      tel_mobile:     "0" + Faker::Number.number(9).to_s,
-                      tel_home:       maybe("0" + Faker::Number.number(9).to_s),
-                      tel_office:     maybe("0" + Faker::Number.number(9).to_s),
-                      # status:         "", # Use default
-                      notes:          maybe(Faker::Lorem.sentence),
-                      activated:      false,
-                      activated_at:   nil)
+  user = User.new(first_name:     first_name,
+                  last_name:      last_name,
+                  email:          email,
+                  postal_address: Faker::Address.street_address,
+                  postal_code:    Faker::Number.number(4).to_s,
+                  city:           Faker::Address.city,
+                  # country:        "", # Use default
+                  tel_mobile:     "0" + Faker::Number.number(9).to_s,
+                  tel_home:       maybe("0" + Faker::Number.number(9).to_s),
+                  tel_office:     maybe("0" + Faker::Number.number(9).to_s),
+                  # status:         "", # Use default
+                  notes:          maybe(Faker::Lorem.sentence),
+                  activated:      false,
+                  activated_at:   nil)
+  user.save(validate: false)
   puts user.inspect
 end
 
 puts
 puts "DB has now #{User.count} users."
+puts
+puts "NB: Model validations were skipped for seeding, since the adresses are" +
+     " not valid."
 puts
 puts "******** Seeding Data End ************"
 puts
