8a532c14ad545e1259f432b3883c38d72c81d75a
rbf <rbf@users.noreply.github.com>
2016-01-25 20:39:19 +0100

    new: make job table rows clickable

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,4 +2,7 @@ jQuery( function() { ( function( $$, $, undefined ) {
   // Initialize Bootstrap popovers
   // Source: http://getbootstrap.com/javascript/#popovers
   $('[data-toggle=popover]').popover();
+  $('tr[data-href]').click( function() {
+    document.location = $(this).data('href');
+  });
 }( window.Basimilch = window.Basimilch || {}, jQuery ));});
