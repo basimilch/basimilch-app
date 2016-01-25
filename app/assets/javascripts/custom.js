b4679586d30d58bf28c98fb8497840ec3ae054dd
rbf <rbf@users.noreply.github.com>
2016-01-25 22:45:38 +0100

    new: add page-loading progress bar

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,7 @@
 // DOC: https://github.com/rails/turbolinks#events
 // DOC: http://stackoverflow.com/a/19834224/5764181
 $(document).on('ready page:load', function() { ( function( $$, $, undefined ) {
+  Turbolinks.enableProgressBar();
   // Initialize Bootstrap popovers
   // Source: http://getbootstrap.com/javascript/#popovers
   $('[data-toggle=popover]').popover();
