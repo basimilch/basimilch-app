59d63832569d4f6f7bbaba67858b80dc59a87028
rbf <rbf@users.noreply.github.com>
2016-02-07 23:47:01 +0100

    minor: add reference code to automatically require all .rb files in a directory

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,12 @@
 require File.expand_path('../application', __FILE__)
 require 'rails_extensions'
 
+# TODO: Outcomment to automatically require all .rb files in lib/
+# SOURCE: http://stackoverflow.com/a/735458
+# Dir[File.expand_path(File.join('..', '..', 'lib', '*.rb'), __FILE__)].each do |file|
+#   puts "Requiring: #{file}"
+#   require file
+# end
+
 # Initialize the Rails application.
 Rails.application.initialize!
