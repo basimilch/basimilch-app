c95736c1d6fe5dd2b8f7fcfcb490bee941546491
rbf <rbf@users.noreply.github.com>
2016-02-28 12:38:30 +0100

    fix: admin mailer test

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -7,6 +7,6 @@ class AdminMailerTest < ActionMailer::TestCase
     assert_equal "Daily activity report", mail.subject
     assert_equal ["daily_activity_report@example.org"], mail.to
     assert_equal ["noreply@example.org"], mail.from
-    assert_match "There was no activity recorded yesterday", mail.body.encoded
+    assert_match "No activity recorded", mail.body.encoded
   end
 end
