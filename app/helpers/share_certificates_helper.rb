31f3823f9ff41bc2d853abca319d32806360005d
rbf <rbf@users.noreply.github.com>
2016-03-06 12:03:59 +0100

    new: enhance share certificates UI
    - include display of creation date of share certificates in all places
    - don't allow to modify the share certificate dates that have already
      been setup (only a UI limitation, not a model limitation to allow
      corrections)

--- app/helpers/share_certificates_helper.rb
+++ app/helpers/share_certificates_helper.rb
@@ -1,2 +1,18 @@
 module ShareCertificatesHelper
+
+def date_field(form, field_name)
+  content_tag(:div, class: "field date-field") do
+    concat form.label(field_name)
+    if date = form.object.try(field_name)
+      # Do not allow to edit the dates that are already set up.
+      concat content_tag(:div, date.to_date.to_localized_s(:long))
+    else
+      concat form.date_select(field_name,
+                              include_blank: true,
+                              default: nil,
+                              start_year: 2015)
+    end
+  end
+end
+
 end
