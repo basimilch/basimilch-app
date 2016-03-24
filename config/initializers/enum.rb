b90dd0d8807196de39110eece7637a266a5594ad
rbf <rbf@users.noreply.github.com>
2016-03-24 16:39:12 +0100

    new: enhance Enum class
    Among other enhancements, Enum now extends Enumerable and includes
    Comparable. See tests for usage.

--- config/initializers/enum.rb
+++ config/initializers/enum.rb
@@ -9,42 +9,77 @@
 #   end
 class Enum
 
+  # DOC: http://ruby-doc.org/core-2.2.4/Enumerable.html
+  # SOURCE: http://stackoverflow.com/q/17552915
+  extend Enumerable
+  def self.each(&block)
+    all.each(&block)
+  end
+
+  # DOC: http://ruby-doc.org/core-2.2.4/Comparable.html
+  include Comparable
+  def <=>(o)
+    to_i <=> self.class.enum_for(o).to_i if self.class.member? o
+  end
+
+  # Class methods
+
+  # TODO: Consider renaming :enum to :declare.
   def self.enum(name)
     unless name == name.upcase && name.is_a?(Symbol)
       raise NameError, "Enum name must be upper case symbol: #{name}", caller
     end
-    if self.valid? name
+    if member? name
       raise NameError, "Enum #{self}::#{name} already defined", caller
     end
     # DOC: http://ruby-doc.org/core-2.2.1/Module.html#method-i-const_set
     # SOURCE: http://stackoverflow.com/a/12426510/5764181
-    (@all ||= []) << const_set(name, new(name.to_s.downcase))
+    all << const_set(name, new(name))
   end
 
-  def self.valid?(name)
-    # DOC: http://ruby-doc.org/core-2.2.1/Module.html#method-i-constant_defined
-    const_defined? name.upcase.to_sym, false rescue false
-  end
-
+  # Returns effectively the same result than Enumerable#entries but without
+  # going through the Enumerable#each function.
   def self.all
-    @all
+    @all ||= []
   end
+
+  def self.enum_for(o)
+    case o
+    when self
+      o
+    else
+      self.find{|e| e == o}
+    end
+  end
+
+  # Instance methods
 
   def initialize(value)
-    @value = value.to_s
+    @value = value.to_s.downcase
   end
 
   def to_s
-    @value
+    "#{@value}"
   end
 
-  # Enums can be compared to themselves or to their value string.
+  def to_sym
+    @value.upcase.to_sym
+  end
+
+  def to_i
+    @index ||= self.class.find_index self
+  end
+
+  # Enums can be compared to themselves, to their value string or to their
+  # defining symbol
   # SOURCE: http://stackoverflow.com/a/1964155
   def ==(o)
     # SOURCE: http://stackoverflow.com/a/3801609
     case o
     when String
-      o == @value
+      o.downcase == @value
+    when Symbol
+      o.to_s.downcase == @value
     when self.class
       o.to_s == @value
     else
--- test/initializers/enum_test.rb
+++ test/initializers/enum_test.rb
@@ -11,55 +11,73 @@ class EnumTest < ActionController::TestCase
   end
 
   test "enum should work" do
+
     assert_equal Severity::LOW, Severity::LOW
+    assert_equal true, Severity::LOW == Severity::LOW
     assert_equal true, Severity::LOW == "low"
+    assert_equal true, Severity::LOW == "loW"
+    assert_equal true, Severity::LOW == "LOW"
+    assert_equal true, Severity::LOW == :LOW
+    assert_equal true, Severity::LOW == :LOw
+    assert_equal true, Severity::LOW == :low
 
     assert_not_equal Severity::MEDIUM, Severity::LOW
+    assert_equal false, Severity::LOW == Severity::MEDIUM
     assert_equal false, Severity::LOW == "medium"
     assert_equal false, Severity::LOW == ""
     assert_equal false, Severity::LOW == nil
-
-    assert_equal true, Severity.valid?("low")
-    assert_equal true, Severity.valid?("medium")
-
-    assert_equal false, Severity.valid?("asfd")
-    assert_equal false, Severity.valid?("")
-    assert_equal false, Severity.valid?(nil)
-
-    assert_raise ArgumentError do
-      Severity.valid?()
-    end
 
     assert_equal [
       Severity::LOW,
       Severity::MEDIUM,
       Severity::HIGH,
-      Severity::CRITICAL,
+      Severity::CRITICAL
       ], Severity.all
 
-    # It's a sorted array
+    # It's an array, so it's sorted!
     assert_not_equal [
       Severity::MEDIUM,
       Severity::CRITICAL,
       Severity::HIGH,
-      Severity::LOW,
+      Severity::LOW
       ], Severity.all
+
+    # NOTE: To add an enum to an existing class, you can either re-open the
+    #       class or call the class method :enum. The latter is preferred.
 
     assert_raise NameError do
       # Severity::LOW is already defined and cannot be added
-      class Severity < Enum
+      class Severity
         enum :LOW
       end
     end
+    assert_raise NameError do
+      # Severity::LOW is already defined and cannot be added
+      Severity.enum :LOW
+    end
     assert_equal Severity::CRITICAL, Severity.all.last
 
+    assert_raise NameError do
+      # Severity::UNKNOWN is not already defined
+      Severity::UNKNOWN
+    end
     assert_nothing_raised do
-      # Severity::UNKNOWN is not already defined and can be added
-      class Severity < Enum
+      # Severity::UNKNOWN can be defined
+      class Severity
         enum :UNKNOWN
       end
     end
     assert_equal Severity::UNKNOWN, Severity.all.last
+
+    assert_raise NameError do
+      # Severity::CUSTOM is not already defined
+      Severity::CUSTOM
+    end
+    assert_nothing_raised do
+      # Severity::CUSTOM can be defined
+      Severity.enum :CUSTOM
+    end
+    assert_equal Severity::CUSTOM, Severity.all.last
 
     # The value should not be accessible from outside.
     assert_raise NoMethodError do
@@ -67,6 +85,152 @@ class EnumTest < ActionController::TestCase
     end
     assert_raise NoMethodError do
       Severity::LOW.value = 123
+    end
+
+    assert_equal Severity::LOW, Severity.enum_for(Severity::LOW)
+    assert_equal Severity::LOW, Severity.enum_for("low")
+    assert_equal Severity::LOW, Severity.enum_for("LOW")
+    assert_equal Severity::LOW, Severity.enum_for(:LOW)
+    assert_equal Severity::LOW, Severity.enum_for(:low)
+
+    assert_equal nil, Severity.enum_for(:lo)
+    assert_equal nil, Severity.enum_for("lo")
+    assert_equal nil, Severity.enum_for("")
+    assert_equal nil, Severity.enum_for(1)
+    assert_equal nil, Severity.enum_for(nil)
+
+    # The value returned by :to_s shouldn't be modifiable.
+    Severity::LOW.to_s.upcase!
+    assert_equal "low", Severity::LOW.to_s
+
+    assert_equal 0, Severity::LOW.to_i
+    assert_equal 1, Severity::MEDIUM.to_i
+    assert_equal 2, Severity::HIGH.to_i
+    assert_equal 3, Severity::CRITICAL.to_i
+
+    # The value returned by :to_i shouldn't be modifiable.
+    assert_equal 1, Severity::MEDIUM.to_i
+    assert_equal 2, Severity::MEDIUM.to_i.next
+    assert_equal 1, Severity::MEDIUM.to_i
+    assert_equal 2, Severity::MEDIUM.to_i << 1
+    assert_equal 1, Severity::MEDIUM.to_i
+
+    # Enum class extends Enumerable
+
+    assert_equal [
+      "low",
+      "medium",
+      "high",
+      "critical",
+      "unknown",
+      "custom"
+      ], Severity.map(&:to_s)
+    assert_equal [
+      :LOW,
+      :MEDIUM,
+      :HIGH,
+      :CRITICAL,
+      :UNKNOWN,
+      :CUSTOM
+      ], Severity.map(&:to_sym)
+    assert_equal 6, Severity.count
+    assert_equal Severity.all, Severity.entries # Although :all is faster
+    assert_equal Severity.all, Severity.to_a    # Although :all is faster
+    assert_equal Severity.all.to_set, Severity.to_set
+    assert_equal true,  Severity.member?("low")
+    assert_equal true,  Severity.member?("LOW")
+    assert_equal true,  Severity.member?(:LOW)
+    assert_equal true,  Severity.member?(:low)
+    assert_equal true,  Severity.member?(Severity::LOW)
+    assert_equal false, Severity.member?("")
+    assert_equal false, Severity.member?(nil)
+
+    # With Enumerable#to_set we can compare the unsorted list of enums.
+    assert_equal Severity.all.shuffle.to_set, Severity.to_set
+    assert_equal [
+      Severity::LOW,
+      Severity::HIGH,
+      Severity::CUSTOM,
+      Severity::MEDIUM,
+      Severity::UNKNOWN,
+      Severity::CRITICAL
+      ].to_set, Severity.to_set
+
+    assert_equal Severity::HIGH,  Severity.find{|e| e.to_s.chr == "h"}
+
+    # Enum class includes Comparable
+
+    assert_equal Severity::LOW, Severity.min
+    assert_equal Severity::CUSTOM, Severity.max
+
+    assert_equal true,  Severity::MEDIUM <  Severity::HIGH
+    assert_equal true,  Severity::MEDIUM <= Severity::HIGH
+    assert_equal false, Severity::MEDIUM == Severity::HIGH
+    assert_equal false, Severity::MEDIUM >= Severity::HIGH
+    assert_equal false, Severity::MEDIUM >  Severity::HIGH
+
+    assert_equal false, Severity::MEDIUM <  Severity::MEDIUM
+    assert_equal true,  Severity::MEDIUM <= Severity::MEDIUM
+    assert_equal true,  Severity::MEDIUM == Severity::MEDIUM
+    assert_equal true,  Severity::MEDIUM >= Severity::MEDIUM
+    assert_equal false, Severity::MEDIUM >  Severity::MEDIUM
+
+    assert_equal false, Severity::MEDIUM <  Severity::LOW
+    assert_equal false, Severity::MEDIUM <= Severity::LOW
+    assert_equal false, Severity::MEDIUM == Severity::LOW
+    assert_equal true,  Severity::MEDIUM >= Severity::LOW
+    assert_equal true,  Severity::MEDIUM >  Severity::LOW
+
+    assert_equal true,  Severity::MEDIUM <  "high"
+    assert_equal true,  Severity::MEDIUM <= "high"
+    assert_equal false, Severity::MEDIUM == "high"
+    assert_equal false, Severity::MEDIUM >= "high"
+    assert_equal false, Severity::MEDIUM >  "high"
+
+    assert_equal false, Severity::MEDIUM <  "medium"
+    assert_equal true,  Severity::MEDIUM <= "medium"
+    assert_equal true,  Severity::MEDIUM == "medium"
+    assert_equal true,  Severity::MEDIUM >= "medium"
+    assert_equal false, Severity::MEDIUM >  "medium"
+
+    assert_equal false, Severity::MEDIUM <  "low"
+    assert_equal false, Severity::MEDIUM <= "low"
+    assert_equal false, Severity::MEDIUM == "low"
+    assert_equal true,  Severity::MEDIUM >= "low"
+    assert_equal true,  Severity::MEDIUM >  "low"
+
+    assert_equal true,  Severity::MEDIUM <  :HIGH
+    assert_equal true,  Severity::MEDIUM <= :HIGH
+    assert_equal false, Severity::MEDIUM == :HIGH
+    assert_equal false, Severity::MEDIUM >= :HIGH
+    assert_equal false, Severity::MEDIUM >  :HIGH
+
+    assert_equal false, Severity::MEDIUM <  :MEDIUM
+    assert_equal true,  Severity::MEDIUM <= :MEDIUM
+    assert_equal true,  Severity::MEDIUM == :MEDIUM
+    assert_equal true,  Severity::MEDIUM >= :MEDIUM
+    assert_equal false, Severity::MEDIUM >  :MEDIUM
+
+    assert_equal false, Severity::MEDIUM <  :LOW
+    assert_equal false, Severity::MEDIUM <= :LOW
+    assert_equal false, Severity::MEDIUM == :LOW
+    assert_equal true,  Severity::MEDIUM >= :LOW
+    assert_equal true,  Severity::MEDIUM >  :LOW
+
+    assert_raise ArgumentError do
+      Severity::LOW < nil
+    end
+
+    assert_raise ArgumentError do
+      Severity::LOW < :HIG
+    end
+
+    assert_raise ArgumentError do
+      Severity::LOW < "hig"
+    end
+
+    assert_raise ArgumentError do
+      Severity::LOW < 1
     end
   end
 end
