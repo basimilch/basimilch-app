9fa0105c8b9f8885920bb6c816b76e89be9ef703
rbf <rbf@users.noreply.github.com>
2016-04-13 22:14:49 +0200

    new: implement email blacklisting
    This is specially useful to ensure that no email is delivered to placeholder
    addresses like @example.org in prod.

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -21,43 +21,87 @@ ActionMailer::Base.register_interceptor(AddAppPrefixToEmailSubject)
 
 
 # SOURCE: http://renderedtext.com/blog/2012/04/27/filtering-emails-on-staging/
-class RecipientWhitelistInterceptor
+class RecipientWhitelistBlacklistInterceptor
 
-  def self.delivering_email(message)
-    whitelist = ENV['EMAIL_RECIPIENTS_WHITELIST']
-    return unless whitelist
-    ActionMailer::Base.logger.info "Checking email recipients against whitelist:"
-    original_recipients = message.to
-    whitelist_regexp    = regex_for_whitelist(whitelist)
-    allowed_recipients  = original_recipients.select do |address|
-      allowed_address? address, whitelist_regexp
+  # TODO: Consider refactoring the logic related to RegExps in this class out to
+  #       a file like regexp_util.rb.
+
+  # SOURCE: https://www.ruby-forum.com/topic/49046
+  MATCH_EVERYTHING = Regexp.new('')
+  MATCH_NOTHING    = Regexp.new('.^')
+
+  def self.regex_for_email_list(email_list)
+    return nil            if email_list.nil?
+    return MATCH_NOTHING  if email_list.strip.empty?
+    email_addresses = email_list.split(',').map do |address|
+      # Match all emails addresses of a domain, if only the domain is given
+      # Match the + aliases for gmail addresses
+      # TODO: If a gmail alias is explicitly given, consider matching only
+      #       this alias, not the base email address or other aliases.
+      address
+        .strip
+        .downcase
+        .gsub(/([^+]+)(\+.*:?)?@gmail.com/,'\1(\\\\+.*:?)?@gmail.com')
+        .gsub(/^(@\S+\.[a-z]{2,})$/, '\\S+\1')
     end
-    ActionMailer::Base.logger.info " - Whitelist: #{whitelist.inspect}"
-    ActionMailer::Base.logger.info " - Whitelist Regexp: #{whitelist_regexp.inspect}"
-    ActionMailer::Base.logger.info " - Original recipients: #{original_recipients.inspect}"
-    ActionMailer::Base.logger.info " - Allowed recipients: #{allowed_recipients.inspect}"
-    message.to = allowed_recipients
-    if message.to.empty?
-      ActionMailer::Base.logger.info "Email will not be delivered."
-      message.perform_deliveries = false
+    Regexp.new("^(#{email_addresses.join("|")}:?)$")
+  end
+
+  WHITELIST        = ENV['EMAIL_RECIPIENTS_WHITELIST']
+  BLACKLIST        = ENV['EMAIL_RECIPIENTS_BLACKLIST']
+  WHITELIST_REGEXP = regex_for_email_list(WHITELIST)
+  BLACKLIST_REGEXP = regex_for_email_list(BLACKLIST)
+
+
+  # Returns a new array containing all email addresses in the 'original_list'
+  # array which are allowed given the current WHITELIST and BLACKLIST.
+  #
+  # NOTE: The meaning of 'nil' for whitelist and blacklist is the
+  #       same: they are 'nil' when they have not been explicitly set
+  #       up in an ENV variable and thus they have no influence on the
+  #       allowed email filtering, i.e. they don't block any email
+  #       addresses.
+  #
+  #       However they can be setup (i.e. they will not be 'nil') to
+  #       an empty string (i.e. list && list.strip.empty?). In that
+  #       case the meaning will differ. A not-nil but empty blacklist
+  #       is the same than a 'nil' blacklist: it does not have any
+  #       influence on the filtering of email addresses. However, a
+  #       not-nil but empty whitelist has the contrary meaning: it
+  #       blocks all addresses because none are in the allowed
+  #       whitelist. This might be a bit confusing, and thus all this
+  #       cases are checked in the tests for clarity.
+  #
+  #       Blacklist rules over whitelist, i.e. if an email address is
+  #       both whitelisted and blacklisted it will be blocked.
+  def self.select_allowed_addresses(original_list,
+                                    whitelist_regexp: nil,
+                                    blacklist_regexp: nil)
+    original_list.select do |address|
+      address.match(whitelist_regexp || MATCH_EVERYTHING) &&
+        !address.match(blacklist_regexp || MATCH_NOTHING)
     end
   end
 
-  private
-    def self.regex_for_whitelist(whitelist)
-      email_addresses = whitelist.split(',').map do |address|
-        # Automatically accept the + for gmail addresses
-        # Accept all emails addresses of a domain, if only the domain is given
-        address
-          .strip
-          .downcase
-          .gsub(/([^+]+)(\+.*:?)?@gmail.com/,'\1(\\\\+.*:?)?@gmail.com')
-          .gsub(/^(@\S+\.[a-z]{2,})$/, '\\S+\1')
-      end
-      Regexp.new("^(#{email_addresses.join("|")}:?)$")
+  def self.delivering_email(message)
+    return unless WHITELIST || BLACKLIST
+    logger = ActionMailer::Base.logger
+    logger.info "Checking email recipients against whitelist/blacklist:"
+    original_recipients = message.to
+    allowed_recipients  = select_allowed_addresses original_recipients,
+                            whitelist_regexp: WHITELIST_REGEXP,
+                            blacklist_regexp: BLACKLIST_REGEXP
+    logger.info   " - Whitelist:        #{WHITELIST.inspect}"
+    logger.debug  " - Whitelist Regexp: #{WHITELIST_REGEXP.inspect}"
+    logger.info   " - Blacklist:        #{BLACKLIST.inspect}"
+    logger.debug  " - Blacklist Regexp: #{BLACKLIST_REGEXP.inspect}"
+    logger.info   " - Original recipients:  #{original_recipients.inspect}"
+    logger.info   " - Allowed recipients:   #{allowed_recipients.inspect}"
+    message.to = allowed_recipients
+    if message.to.empty?
+      logger.warn "Email will not be delivered."
+      message.perform_deliveries = false
     end
-    def self.allowed_address?(address, whitelist_regexp)
-      address.match(whitelist_regexp)
-    end
+  end
 end
-ActionMailer::Base.register_interceptor(RecipientWhitelistInterceptor)
+ActionMailer::Base.register_interceptor(RecipientWhitelistBlacklistInterceptor)
---
+++ test/initializers/mailer_test.rb
@@ -0,0 +1,136 @@
+require 'test_helper'
+
+# TODO: Should not be an Action Controller test, but a Unit Test instead.
+class MailerTest < ActionController::TestCase
+
+  setup do
+    @no_address         = []
+    @example_com        = ["a@example.com"]
+    @example_org        = ["a@example.org"]
+    @gmail_com_a_base   = ["a@gmail.com"]
+    @gmail_com_a_alias  = ["a+b@gmail.com"]
+    @gmail_com_a        = @gmail_com_a_base + @gmail_com_a_alias
+    @gmail_com_b        = ["b@gmail.com"]
+    @gmail_adresses     = @gmail_com_a + @gmail_com_b
+    @all_adresses       = @example_com + @example_org + @gmail_adresses
+  end
+
+  # TODO: Separate this tests for a purely Regexp utils test, and properly
+  #       test the email delivery itself with:
+  #       "assert_no_difference 'ActionMailer::Base.deliveries.size' do..."
+
+  # Test the 4 'nil' vs empty string cases
+  # NOTE: Read NOTE on RecipientWhitelistInterceptor.regex_for_email_list
+
+  test "whitelist and blacklist for emails should work 1" do
+    assert_email_filter whitelist:    nil,
+                        blacklist:    nil,
+                        allowed_list: @all_adresses
+  end
+
+  test "whitelist and blacklist for emails should work 2" do
+    assert_email_filter whitelist:    "",
+                        blacklist:    nil,
+                        allowed_list: @no_address
+  end
+
+  test "whitelist and blacklist for emails should work 3" do
+    assert_email_filter whitelist:    nil,
+                        blacklist:    "",
+                        allowed_list: @all_adresses
+  end
+
+  test "whitelist and blacklist for emails should work 4" do
+    assert_email_filter whitelist:    "",
+                        blacklist:    "",
+                        allowed_list: @no_address
+  end
+
+
+  # Test other combinations
+
+  test "whitelist and blacklist for emails should work 5" do
+    assert_email_filter whitelist:    "@gmail.com",
+                        blacklist:    nil,
+                        allowed_list: @gmail_adresses
+  end
+
+  test "whitelist and blacklist for emails should work 6" do
+    assert_email_filter whitelist:    "a@gmail.com",
+                        blacklist:    nil,
+                        allowed_list: @gmail_com_a_base + @gmail_com_a_alias
+  end
+
+  test "whitelist and blacklist for emails should work 7" do
+    assert_email_filter whitelist:    "@gmail.com",
+                        blacklist:    "b@gmail.com",
+                        allowed_list: @gmail_com_a_base + @gmail_com_a_alias
+  end
+
+  test "whitelist and blacklist for emails should work 8" do
+    assert_email_filter whitelist:    " @gmail.com  ",
+                        blacklist:    "a@gmail.com",
+                        allowed_list: @gmail_com_b
+  end
+
+  test "whitelist and blacklist for emails should work 9" do
+    assert_email_filter whitelist:    "@example.com  ",
+                        blacklist:    nil,
+                        allowed_list: @example_com
+  end
+
+  test "whitelist and blacklist for emails should work 10" do
+    assert_email_filter whitelist:    "@example.com, @example.org, a@gmail.com",
+                        blacklist:    nil,
+                        allowed_list: @all_adresses - @gmail_com_b
+  end
+
+  test "whitelist and blacklist for emails should work 11" do
+    assert_email_filter whitelist:    "@example.com,@example.org,a+b@gmail.com",
+                        blacklist:    nil,
+                        allowed_list: @all_adresses - @gmail_com_b
+  end
+
+  test "whitelist and blacklist for emails should work 12" do
+    assert_email_filter whitelist:    "@example.com, @example.org, a@gmail.com",
+                        blacklist:    "a@gmail.com",
+                        allowed_list: @example_com + @example_org
+  end
+
+  test "whitelist and blacklist for emails should work 13" do
+    assert_email_filter whitelist:    "@example.com, @example.org, a@gmail.com",
+                        blacklist:    "a+b@gmail.com",
+                        allowed_list: @example_com + @example_org
+  end
+
+  test "whitelist and blacklist for emails should work 14" do
+    assert_email_filter whitelist:    nil,
+                        blacklist:    "a+b@gmail.com",
+                        allowed_list: @all_adresses - @gmail_com_a
+  end
+
+  test "whitelist and blacklist for emails should work 15" do
+    assert_email_filter whitelist:    nil,
+                        blacklist:    "@gmail.com",
+                        allowed_list: @all_adresses - @gmail_adresses
+  end
+
+  test "whitelist and blacklist for emails should work 16" do
+    assert_email_filter whitelist:    nil,
+                        blacklist:    "@example.com, @example.org",
+                        allowed_list: @gmail_adresses
+  end
+
+  private
+
+    def assert_email_filter(whitelist: nil, blacklist: nil, allowed_list: [])
+      interceptor_class = RecipientWhitelistBlacklistInterceptor
+      wl_regexp = interceptor_class.regex_for_email_list(whitelist)
+      bl_regexp = interceptor_class.regex_for_email_list(blacklist)
+      assert_equal allowed_list, interceptor_class.select_allowed_addresses(
+                                                    @all_adresses,
+                                                    whitelist_regexp: wl_regexp,
+                                                    blacklist_regexp: bl_regexp
+                                                  )
+    end
+end
