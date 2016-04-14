d97f3652e4b0157872a902856bc212f942345e43
rbf <rbf@users.noreply.github.com>
2016-04-14 09:09:10 +0200

    change: now :to_b does never raise an error and the new :to_b! does
    :to_b is changed to never raise an exception and return nil if an object
    cannot successfully be parsed to a boolean. The newly added :to_b!
    instead does raise the exception

--- config/initializers/boolean_typecasting.rb
+++ config/initializers/boolean_typecasting.rb
@@ -1,27 +1,36 @@
-# Source: http://drawingablank.me/blog/ruby-boolean-typecasting.html
+# SOURCE: http://drawingablank.me/blog/ruby-boolean-typecasting.html
+
+class BooleanTypecastError < RuntimeError
+end
 
 class Object
   def to_b
     true
   end
+  def to_b!
+    res = to_b
+    if res.nil?
+      raise BooleanTypecastError, "Cannot cast #{self.inspect} as Boolean"
+    else
+      return res
+    end
+  end
 end
 
 class String
   def to_b
-    return true if self == true ||
-                      self =~ (/^(true|t|yes|y|1)$/i)
-    return false if self == false ||
-                      self.blank? ||
-                      self =~ (/^(false|f|no|n|0)$/i)
-    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+    return true   if self == true ||
+                       self =~ (/^(true|t|yes|y|1)$/i)
+    return false  if self == false ||
+                       self.blank? ||
+                       self =~ (/^(false|f|no|n|0)$/i)
   end
 end
 
 class Fixnum
   def to_b
-    return true if self == 1
-    return false if self == 0
-    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+    return true   if self == 1
+    return false  if self == 0
   end
 end
 
