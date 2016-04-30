e7df5dc88dbb25e18345b462d893f79dcc1c750a
rbf <rbf@users.noreply.github.com>
2016-04-30 14:23:20 +0200

    fix: don't display depot coordinator tel_mobile if not present

--- app/models/depot_coordinator.rb
+++ app/models/depot_coordinator.rb
@@ -8,9 +8,17 @@ class DepotCoordinator < ActiveRecord::Base
   belongs_to :user
   belongs_to :depot
 
+  before_save :check_if_tel_mobile_present
+
   default_scope -> { order(id: :asc) }
 
   def to_s
     "Depot coordinator #{id.inspect}: #{user}"
   end
+
+  private
+    def check_if_tel_mobile_present
+      self.publish_tel_mobile = publish_tel_mobile && user.tel_mobile.present?
+      return true # don't stop the save action
+    end
 end
