37f94d54bf698510898ceaefaa57bbc7f41d711a
rbf <rbf@users.noreply.github.com>
2018-09-02 10:06:43 +0900

    refactor: enhance logging when requiring lib extensions

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,10 @@ require_relative 'application'
 # Automatically require all extension ruby files in lib folder.
 # SOURCE: http://stackoverflow.com/a/735458
 Dir[Rails.root.join('lib', '*_extensions.rb')].each do |file|
-  puts "Requiring #{file}"
+  puts "Requiring #{file}..."
   require file
+end.count.tap do |c|
+  puts "#{c} #{'file'.pluralize(c)} successfully loaded." if c.pos?
 end
 
 # Initialize the Rails application.
